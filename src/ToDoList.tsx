76e08c34fc632fa185da4b1dcd97bb3e1f87436f
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-04 18:04:19 +0900

    #6.6
    useForm( ) 으로 여러가지 Form을 쉽게 처리하기 ( + react-hook-form )

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+
+  console.log(watch());
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form>
+        <input {...register("Email")} placeholder="Email" />
+        <input {...register("First_name")} placeholder="First name" />
+        <input {...register("Last_name")} placeholder="Last name" />
+        <input {...register("User_name")} placeholder="User name" />
+        <input {...register("password1")} placeholder="password1" />
+        <input {...register("password2")} placeholder="password2" />
         <button>Add</button>
       </form>
     </div>
