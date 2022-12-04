e6b41be7779c8569e0c1da406c1b1ee19c483006
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-05 01:58:31 +0900

    #6.7
    useForm( )의 여러가지 기능들

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,19 +2,51 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const { register, watch } = useForm();
-
-  console.log(watch());
+  const { register, watch, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
 
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("First_name")} placeholder="First name" />
-        <input {...register("Last_name")} placeholder="Last name" />
-        <input {...register("User_name")} placeholder="User name" />
-        <input {...register("password1")} placeholder="password1" />
-        <input {...register("password2")} placeholder="password2" />
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("Email", {
+            required: { value: true, message: "Email is reuqired." },
+          })}
+          placeholder="Email"
+        />
+        <input
+          {...register("First_name", {
+            required: { value: true, message: "First name is reuqired." },
+          })}
+          placeholder="First name"
+        />
+        <input
+          {...register("Last_name", {
+            required: { value: true, message: "Last name is reuqired." },
+            minLength: 2,
+          })}
+          placeholder="Last name"
+        />
+        <input
+          {...register("User_name", {
+            required: { value: true, message: "User name is reuqired." },
+          })}
+          placeholder="User name"
+        />
+        <input
+          {...register("password1", {
+            required: { value: true, message: "Password is reuqired." },
+          })}
+          placeholder="password1"
+        />
+        <input
+          {...register("password2", {
+            required: { value: true, message: "Password is reuqired." },
+          })}
+          placeholder="password2"
+        />
         <button>Add</button>
       </form>
     </div>
