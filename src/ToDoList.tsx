729a83e66245e139ae282c1a21410396d63ff317
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-05 16:22:12 +0900

    #6.8
    regular Express를 통한 register의 유효성검사
    useForm( )으로 각 register에 기본값 설정

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,8 +1,22 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  Email: string;
+  First_name: string;
+  Last_name: string;
+  User_name: string;
+  password1: string;
+  password2: string;
+}
+
 function ToDoList() {
-  const { register, watch, handleSubmit, formState } = useForm();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({ defaultValues: { Email: "@naver.com" } });
   const onValid = (data: any) => {
     console.log(data);
   };
@@ -13,6 +27,10 @@ function ToDoList() {
         <input
           {...register("Email", {
             required: { value: true, message: "Email is reuqired." },
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/gm,
+              message: "Only naver.com emails allowed",
+            },
           })}
           placeholder="Email"
         />
@@ -25,7 +43,7 @@ function ToDoList() {
         <input
           {...register("Last_name", {
             required: { value: true, message: "Last name is reuqired." },
-            minLength: 2,
+            minLength: { value: 4, message: "Last name minLength is 4." },
           })}
           placeholder="Last name"
         />
@@ -49,6 +67,12 @@ function ToDoList() {
         />
         <button>Add</button>
       </form>
+      <p>{errors?.Email?.message}</p>
+      <p>{errors?.First_name?.message}</p>
+      <p>{errors?.Last_name?.message}</p>
+      <p>{errors?.User_name?.message}</p>
+      <p>{errors?.password1?.message}</p>
+      <p>{errors?.password2?.message}</p>
     </div>
   );
 }
