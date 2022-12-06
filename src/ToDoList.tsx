b9235410f8bf33b050329b5f86b043b213399076
jiyong <82662371+jiyong0419@users.noreply.github.com>
2022-12-06 16:40:05 +0900

    #6.10
    useForm의 setValue를 이용하여 register의 value를 변경

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,94 +1,25 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
-interface IForm {
-  Email: string;
-  First_name: string;
-  Last_name: string;
-  User_name: string;
-  password1: string;
-  password2: string;
-  extraError?: string;
+interface ISubmitData {
+  toDo: string;
 }
 
 function ToDoList() {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm<IForm>({ defaultValues: { Email: "@naver.com" } });
-  const onValid = (data: IForm) => {
-    if (data.password1 !== data.password2) {
-      setError("password1", { message: "Password are not the same." });
-    }
+  const { register, handleSubmit, setValue } = useForm<ISubmitData>();
+  const onSubmit = (data: ISubmitData) => {
+    console.log(data.toDo);
+    setValue("toDo", "");
   };
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
         <input
-          {...register("Email", {
-            required: { value: true, message: "Email is reuqired." },
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/gm,
-              message: "Only naver.com emails allowed",
-            },
-          })}
-          placeholder="Email"
+          {...register("toDo", { required: "Write To Do" })}
+          placeholder="Write a to do"
         />
-        <input
-          {...register("First_name", {
-            required: { value: true, message: "First name is reuqired." },
-            validate: {
-              noJeon: (value) =>
-                value.includes("jeon") ? "no Jeon allowed" : true,
-              noChoi: (value) =>
-                value.includes("choi") ? "no Choi allowed" : true,
-            },
-          })}
-          placeholder="First name"
-        />
-        <input
-          {...register("Last_name", {
-            required: { value: true, message: "Last name is reuqired." },
-            minLength: { value: 4, message: "Last name minLength is 4." },
-            validate: {
-              noJiyong: (value) =>
-                value.includes("jiyong") ? "no jiyong allowed." : true,
-              noNico: (value) =>
-                value.includes("nico") ? "no nico allowed." : true,
-            },
-          })}
-          placeholder="Last name"
-        />
-        <input
-          {...register("User_name", {
-            required: { value: true, message: "User name is reuqired." },
-          })}
-          placeholder="User name"
-        />
-        <input
-          {...register("password1", {
-            required: { value: true, message: "Password is reuqired." },
-          })}
-          placeholder="password1"
-        />
-        <input
-          {...register("password2", {
-            required: { value: true, message: "Password is reuqired." },
-          })}
-          placeholder="password2"
-        />
-        <button>Add</button>
+        <button>submit</button>
       </form>
-      <p>{errors?.Email?.message}</p>
-      <p>{errors?.First_name?.message}</p>
-      <p>{errors?.Last_name?.message}</p>
-      <p>{errors?.User_name?.message}</p>
-      <p>{errors?.password1?.message}</p>
-      <p>{errors?.password2?.message}</p>
-      <p>{errors?.extraError?.message}</p>
     </div>
   );
 }
